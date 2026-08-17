869f8ea283c04f93a9ece4cfed3f1488a283d4d1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-18 01:09:33 +0200

    Wrap generated factories that consume mapped provenance

---
+++ src/Resolver/Entry/MappedRequestAwareFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Resolver\Entry;
+
+use Closure;
+use Componenta\Config\ContainerValue;
+
+/** @internal */
+final readonly class MappedRequestAwareFactory implements MappedRequestAwareFactoryInterface
+{
+    /** @param Closure(ContainerValue, array<string|int, mixed>): object $factory */
+    public function __construct(private Closure $factory) {}
+
+    /** @param array<string|int, mixed> $context */
+    public function __invoke(ContainerValue $container, array $context = []): object
+    {
+        return ($this->factory)($container, $context);
+    }
+}
