fb92e511def17b121786b7a022cd4bb81815d651
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-18 14:38:55 +0200

    Apply v5 CI style fixes

--- src/Container.php
+++ src/Container.php
@@ -6,10 +6,7 @@ namespace Componenta\DI;
 
 use Componenta\Config\Config;
 use Componenta\DI\Definition\DefinitionInterface;
-use Componenta\DI\Exception\CircularDependencyException;
-use Componenta\DI\Exception\ConcurrentResolutionException;
 use Componenta\DI\Exception\InvalidConfigurationException;
-use Componenta\DI\Exception\NotFoundException;
 use Componenta\DI\Exception\ResolutionException;
 use Componenta\DI\Resolver\Entry\DefinitionAwareResolverInterface;
 use Componenta\DI\Resolver\Entry\EntryResolverInterface;
