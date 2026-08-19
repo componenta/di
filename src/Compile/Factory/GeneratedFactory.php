f063159542c9a12d10edd0143f83918f00381067
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 23:17:58 +0200

    Remove AOT semantic fast-path metadata

--- src/Compile/Factory/GeneratedFactory.php
+++ src/Compile/Factory/GeneratedFactory.php
@@ -7,14 +7,10 @@ namespace Componenta\DI\Compile\Factory;
 /** Immutable result of compiling one class factory. */
 final readonly class GeneratedFactory
 {
-    /**
-     * @param class-string $class
-     * @param list<class-string>|null $plainAutowireTypes
-     */
+    /** @param class-string $class */
     public function __construct(
         public string $class,
         public string $method,
         public string $code,
-        public ?array $plainAutowireTypes = null,
     ) {}
 }
