74d38e49661b05a1bdbe4d88318951b578c85b6e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:33:31 +0200

    Restore Config source marker

--- src/Attribute/Config.php
+++ src/Attribute/Config.php
@@ -7,10 +7,11 @@ namespace Componenta\DI\Attribute;
 use Attribute;
 use Componenta\Config\ConfigPath;
 use Componenta\Config\DefaultValue;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Provides a value from application configuration. */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class Config
+final readonly class Config implements ParameterSourceAttributeInterface
 {
     public const string KEY = 'config';
 
