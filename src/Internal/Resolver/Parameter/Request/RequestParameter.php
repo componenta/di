3d22511325209b704ed27996ea036c26d2a76022
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 23:53:27 +0200

    Move request transport helper to Internal

---
+++ src/Internal/Resolver/Parameter/Request/RequestParameter.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Internal\Resolver\Parameter\Request;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+/** Internal PSR-7 request transport carried through the ordinary parameter array. @internal */
+final class RequestParameter
+{
+    public const string KEY = ServerRequestInterface::class;
+    public const string PARAMETER_NAME_ATTRIBUTE = "\0componenta.di.parameter-name";
+
+    /** @param array<string|int, mixed> $providedParameters */
+    public static function has(array $providedParameters): bool
+    {
+        return ($providedParameters[self::KEY] ?? null) instanceof ServerRequestInterface;
+    }
+
+    /** @param array<string|int, mixed> $providedParameters */
+    public static function get(array $providedParameters): ?ServerRequestInterface
+    {
+        $request = $providedParameters[self::KEY] ?? null;
+        return $request instanceof ServerRequestInterface ? $request : null;
+    }
+
+    /**
+     * @param array<string|int, mixed> $providedParameters
+     * @return array<string|int, mixed>
+     */
+    public static function with(array $providedParameters, ServerRequestInterface $request): array
+    {
+        $providedParameters[self::KEY] = $request;
+        return $providedParameters;
+    }
+
+    private function __construct() {}
+}
