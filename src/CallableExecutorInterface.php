f54c1d5aff32763a1498990a11c77d158aa3d061
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:07:54 +0200

    Remove context-specific callable execute API

--- src/CallableExecutorInterface.php
+++ src/CallableExecutorInterface.php
@@ -4,11 +4,5 @@ declare(strict_types=1);
 
 namespace Componenta\DI;
 
-/** Resolves callable representations and executes them through DI value resolution. */
-interface CallableExecutorInterface extends CallableInvokerInterface, CallableResolverInterface
-{
-    public function execute(
-        mixed $callable,
-        ResolutionContext $context = new ResolutionContext(),
-    ): mixed;
-}
+/** Resolves callable representations and executes them through DI parameter resolution. */
+interface CallableExecutorInterface extends CallableInvokerInterface, CallableResolverInterface {}
