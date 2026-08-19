093cfcdc112810bcdc3e5008f16470d211152393
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:02:53 +0200

    Restore Cast defaults

--- src/Attribute/Cast.php
+++ src/Attribute/Cast.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace Componenta\DI\Attribute;
 
 use Attribute;
+use Componenta\Config\DefaultValue;
 
-/** Applies a named caster to an already resolved value. */
-#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
+/** Casts a resolved parameter/property value with a named caster. */
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
 final readonly class Cast
 {
-    public function __construct(public string $name) {}
+    public function __construct(
+        public string $name,
+        public mixed $default = DefaultValue::None,
+    ) {}
 }
