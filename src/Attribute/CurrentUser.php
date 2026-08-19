4829d4b67f7c643c521773e91f056e253fe22082
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:33:10 +0200

    Restore CurrentUser source marker

--- src/Attribute/CurrentUser.php
+++ src/Attribute/CurrentUser.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Componenta\DI\Attribute;
 
 use Attribute;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Provides the current authenticated user. */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class CurrentUser
+final readonly class CurrentUser implements ParameterSourceAttributeInterface
 {
     /** @param class-string|null $type */
     public function __construct(public ?string $type = null) {}
