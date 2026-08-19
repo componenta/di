eec22302badb48ce83945d03846a60f743d85452
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:39:32 +0200

    Restore parameter resolver configuration keys

--- src/ConfigKey.php
+++ src/ConfigKey.php
@@ -13,9 +13,14 @@ final class ConfigKey
     public const string ALIASES = \Componenta\Config\ConfigKey::ALIASES;
     public const string DELEGATORS = \Componenta\Config\ConfigKey::DELEGATORS;
     public const string SERVICES = \Componenta\Config\ConfigKey::SERVICES;
+
+    /** priority => resolver instance/service/factory/[service, method] */
+    public const string PARAMETER_RESOLVERS = \Componenta\Config\ConfigKey::PARAMETER_RESOLVERS;
+    public const string PARAMETER_RESOLVERS_REPLACE = \Componenta\Config\ConfigKey::PARAMETER_RESOLVERS_REPLACE;
+
     public const string ATTRIBUTE_DEFINITIONS = 'attribute_definitions';
+    public const string ATTRIBUTE_DEFINITIONS_REPLACE = 'attribute_definitions_replace';
     public const string ATTRIBUTE_CAPABILITIES = 'attribute_capabilities';
-    public const string VALUE_FALLBACKS = 'value_fallbacks';
 
     /** @return list<string> */
     public static function dependencyKeys(): array
@@ -26,9 +31,11 @@ final class ConfigKey
             self::ALIASES,
             self::DELEGATORS,
             self::SERVICES,
+            self::PARAMETER_RESOLVERS,
+            self::PARAMETER_RESOLVERS_REPLACE,
             self::ATTRIBUTE_DEFINITIONS,
+            self::ATTRIBUTE_DEFINITIONS_REPLACE,
             self::ATTRIBUTE_CAPABILITIES,
-            self::VALUE_FALLBACKS,
         ];
     }
 
