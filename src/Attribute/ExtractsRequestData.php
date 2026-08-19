4279ef9ed5d35d947045c3e88b0c48a1fd457d5c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:25:18 +0200

    Restore shared request extraction trait

---
+++ src/Attribute/ExtractsRequestData.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Attribute;
+
+use Componenta\DI\Resolver\Parameter\Request\RequestMapperPipeline;
+use Psr\Http\Message\ServerRequestInterface;
+
+trait ExtractsRequestData
+{
+    /** @var list<string> */
+    protected array $attributes = [];
+    /** @var list<string> */
+    protected array $files = [];
+
+    /** @return array<string,array<string|int,mixed>> */
+    protected function extractSharedSources(ServerRequestInterface $request): array
+    {
+        return [
+            'request attributes' => $this->extractConfiguredRequestAttributes($request),
+            'uploaded files' => $this->extractConfiguredUploadedFiles($request),
+        ];
+    }
+
+    /** @return array<string|int,mixed> */
+    protected function extractSharedData(ServerRequestInterface $request): array
+    {
+        return $this->mergeRequestData($this->extractSharedSources($request));
+    }
+
+    /** @return array<string|int,mixed> */
+    private function extractConfiguredRequestAttributes(ServerRequestInterface $request): array
+    {
+        if ($this->attributes === [RequestMapperPipeline::WILDCARD]) {
+            return $request->getAttributes();
+        }
+        if ($this->attributes === []) {
+            return [];
+        }
+
+        $data = [];
+        $attributes = $request->getAttributes();
+        foreach ($this->attributes as $attribute) {
+            if (array_key_exists($attribute, $attributes)) {
+                $data[$attribute] = $attributes[$attribute];
+            }
+        }
+        return $data;
+    }
+
+    /** @return array<string|int,mixed> */
+    private function extractConfiguredUploadedFiles(ServerRequestInterface $request): array
+    {
+        if ($this->files === [RequestMapperPipeline::WILDCARD]) {
+            return $request->getUploadedFiles();
+        }
+        if ($this->files === []) {
+            return [];
+        }
+
+        $data = [];
+        $files = $request->getUploadedFiles();
+        foreach ($this->files as $key) {
+            if (array_key_exists($key, $files)) {
+                $data[$key] = $files[$key];
+            }
+        }
+        return $data;
+    }
+}
