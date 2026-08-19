295d32aab32551e0433dfe7af90d2fb0105a9ae6
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:33:52 +0200

    Restore EntryId source marker

--- src/Attribute/EntryId.php
+++ src/Attribute/EntryId.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Componenta\DI\Attribute;
 
 use Attribute;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Provides a value from an explicit container entry id. */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class EntryId
+final readonly class EntryId implements ParameterSourceAttributeInterface
 {
     public function __construct(public string $value) {}
 }
