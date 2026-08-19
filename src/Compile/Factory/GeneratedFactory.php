251dea4bd38d15e75b926872aae6f3909831bf02
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 18:16:01 +0200

    Carry compiled fast-path metadata

--- src/Compile/Factory/GeneratedFactory.php
+++ src/Compile/Factory/GeneratedFactory.php
@@ -7,10 +7,14 @@ namespace Componenta\DI\Compile\Factory;
 /** Immutable result of compiling one class factory. */
 final readonly class GeneratedFactory
 {
-    /** @param class-string $class */
+    /**
+     * @param class-string $class
+     * @param list<class-string>|null $plainAutowireTypes
+     */
     public function __construct(
         public string $class,
         public string $method,
         public string $code,
+        public ?array $plainAutowireTypes = null,
     ) {}
 }
