6b5e70f995a4d77d45075d402b750ef996364868
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:33:41 +0200

    Restore Env source marker

--- src/Attribute/Env.php
+++ src/Attribute/Env.php
@@ -6,10 +6,11 @@ namespace Componenta\DI\Attribute;
 
 use Attribute;
 use Componenta\Config\DefaultValue;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Provides a raw environment value. Use #[Cast] for conversion. */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class Env
+final readonly class Env implements ParameterSourceAttributeInterface
 {
     public function __construct(
         public ?string $name = null,
