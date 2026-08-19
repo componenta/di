52daddbe3549e3df2f064493e111c43d16d4d04c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:25:41 +0200

    Restore request mapper base API

---
+++ src/Attribute/RequestMapper.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Attribute;
+
+use Componenta\Caster\CasterProviderAwareInterface;
+use Componenta\Caster\CasterProviderInterface;
+use Componenta\Caster\NullCasterProvider;
+use Componenta\DI\Exception\RequestDataConflictException;
+use Componenta\DI\Resolver\Parameter\Request\MapperInterface;
+use Componenta\DI\Resolver\Parameter\Request\RequestDataConflictPolicy;
+use Componenta\DI\Resolver\Parameter\Request\RequestMapperPipeline;
+
+/** Base declarative transformation contract for all Map* request attributes. */
+abstract class RequestMapper implements MapperInterface, CasterProviderAwareInterface
+{
+    private static ?RequestMapperPipeline $pipeline = null;
+
+    public CasterProviderInterface $provider {
+        get => $this->provider ??= new NullCasterProvider();
+        set(CasterProviderInterface $value) {
+            $this->provider = $value;
+        }
+    }
+
+    /** @var array<string,string> */
+    protected array $cast = [];
+    /** @var array<string,mixed> */
+    protected array $defaults = [];
+    /** @var array<string,array<string,mixed>> */
+    protected array $sortMap = [];
+    /** @var list<string> */
+    protected array $exclude = [];
+    protected RequestDataConflictPolicy $conflictPolicy = RequestDataConflictPolicy::Reject;
+    /** @var array<string,string> */
+    public protected(set) array $map = [];
+
+    /** @param array<string,string> $map */
+    public function __construct(
+        array $map = [],
+        ?RequestDataConflictPolicy $conflictPolicy = null,
+    ) {
+        $this->map = array_merge($this->map, $map);
+        if ($conflictPolicy !== null) {
+            $this->conflictPolicy = $conflictPolicy;
+        }
+    }
+
+    /** @param array<string,array<string|int,mixed>> $sources @return array<string|int,mixed> */
+    protected function mergeRequestData(array $sources): array
+    {
+        $data = [];
+        $owners = [];
+        foreach ($sources as $source => $values) {
+            foreach ($values as $key => $value) {
+                if (!array_key_exists($key, $data)) {
+                    $data[$key] = $value;
+                    $owners[$key] = $source;
+                    continue;
+                }
+                if ($data[$key] === $value
+                    || $this->conflictPolicy === RequestDataConflictPolicy::FirstWins
+                ) {
+                    continue;
+                }
+                throw new RequestDataConflictException(
+                    key: $key,
+                    existingSource: $owners[$key],
+                    incomingSource: $source,
+                );
+            }
+        }
+        return $data;
+    }
+
+    /** @param array<string|int,mixed> $data @return array<string|int,mixed> */
+    public function transform(array $data): array
+    {
+        return (self::$pipeline ??= new RequestMapperPipeline())->run(
+            $data,
+            $this->map,
+            $this->defaults,
+            $this->cast,
+            $this->sortMap,
+            $this->exclude,
+            $this->provider,
+        );
+    }
+}
