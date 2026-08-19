a7132a43c04334d771638dd09d90564c7f96d5e5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 17:35:48 +0200

    Cache constructor targets in object metadata

--- src/Object/ObjectMetadata.php
+++ src/Object/ObjectMetadata.php
@@ -5,14 +5,22 @@ declare(strict_types=1);
 namespace Componenta\DI\Object;
 
 use Componenta\DI\Attribute\Composition\AttributePlan;
+use Componenta\DI\Resolver\Target\ParameterTarget;
 use ReflectionClass;
+use ReflectionMethod;
 
 /** Fully discovered immutable runtime metadata for one object class. */
 final readonly class ObjectMetadata
 {
-    /** @param ReflectionClass<object> $class */
+    /**
+     * @param ReflectionClass<object> $class
+     * @param list<ParameterTarget> $constructorTargets
+     */
     public function __construct(
         public ReflectionClass $class,
         public AttributePlan $classPlan,
+        public ?ReflectionMethod $constructor,
+        public array $constructorTargets,
+        public bool $hasAttributeHandlers,
     ) {}
 }
