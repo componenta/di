78cf22c644eae900af6c38b7ffb9ee5404a869ce
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:28:15 +0200

    Restore QueryParam extractor semantics

--- src/Attribute/QueryParam.php
+++ src/Attribute/QueryParam.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace Componenta\DI\Attribute;
 
-use Attribute;
 use Componenta\Config\DefaultValue;
+use Componenta\DI\Resolver\Parameter\Request\CastableInterface;
+use Componenta\DI\Resolver\Parameter\Request\ExtractorInterface;
+use Componenta\DI\Resolver\Parameter\Request\RequestResolver;
+use Psr\Http\Message\ServerRequestInterface;
 
-#[Attribute(Attribute::TARGET_PARAMETER)]
-final readonly class QueryParam
+#[\Attribute(\Attribute::TARGET_PARAMETER)]
+readonly class QueryParam implements ExtractorInterface, CastableInterface
 {
     public function __construct(
         public ?string $name = null,
         public mixed $default = DefaultValue::None,
+        public ?string $cast = null,
     ) {}
+
+    public function extract(ServerRequestInterface $request): mixed
+    {
+        $name = $this->name ?? $request->getAttribute(RequestResolver::PARAMETER_NAME_ATTRIBUTE);
+        if (!is_string($name) || $name === '') {
+            throw new \LogicException('Query parameter name must be a non-empty string');
+        }
+
+        $params = $request->getQueryParams();
+        if (!array_key_exists($name, $params)) {
+            if ($this->default === DefaultValue::None) {
+                throw new \RuntimeException(sprintf('Required query parameter "%s" is missing', $name));
+            }
+            return $this->default;
+        }
+        return $params[$name];
+    }
 }
