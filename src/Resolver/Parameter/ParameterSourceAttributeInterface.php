424b4ca080821370077cee2835d84b235caa1b31
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:22:40 +0200

    Restore parameter source marker

---
+++ src/Resolver/Parameter/ParameterSourceAttributeInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Resolver\Parameter;
+
+/** Marks a parameter attribute that explicitly declares the value source. */
+interface ParameterSourceAttributeInterface {}
