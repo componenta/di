a24afc107d949fa5723caaf3cec1a18f3ff51460
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:24:12 +0200

    Restore request mapper contract

---
+++ src/Resolver/Parameter/Request/MapperInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\DI\Resolver\Parameter\Request;
+
+interface MapperInterface
+{
+    /** @param array<string|int, mixed> $data @return array<string|int, mixed> */
+    public function transform(array $data): array;
+}
