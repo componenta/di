47ece46ec40350f181cba4b7f8b43435d752dd13
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 20:00:39 +0200

    Document definition compiler exception contract

--- src/Compile/Definition/DefinitionCompilerInterface.php
+++ src/Compile/Definition/DefinitionCompilerInterface.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Componenta\DI\Compile\Definition;
 
+use Componenta\DI\Exception\CompilationException;
+use Componenta\DI\Exception\InvalidConfigurationException;
+
 /** Compiles declarative definitions contained in normalized DI dependencies. */
 interface DefinitionCompilerInterface
 {
     /**
      * @param array<string, mixed> $dependencies
      * @return array<string, mixed>
+     * @throws InvalidConfigurationException Invalid compile configuration.
+     * @throws CompilationException Foreign code-generator failure.
      */
     public function compile(array $dependencies): array;
 }
