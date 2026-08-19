a943d58492ac7dcef6a5e066c07ea53279cd62ef
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 19:20:08 +0200

    Classify attribute composition as configuration failure

--- src/Exception/AttributeCompositionException.php
+++ src/Exception/AttributeCompositionException.php
@@ -4,7 +4,5 @@ declare(strict_types=1);
 
 namespace Componenta\DI\Exception;
 
-use InvalidArgumentException;
-
 /** Raised while immutable DI attribute metadata is being composed into a plan. */
-final class AttributeCompositionException extends InvalidArgumentException implements ExceptionInterface {}
+final class AttributeCompositionException extends InvalidConfigurationException {}
