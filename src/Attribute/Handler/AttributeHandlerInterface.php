33dd81536daed387a5a35370c630f1cfd6217c04
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 03:00:18 +0200

    Remove obsolete v5 value pipeline runtime

--- src/Attribute/Handler/AttributeHandlerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Componenta\DI\Attribute\Handler;
-
-/** Root marker for executable behavior bound by an AttributeDefinition. */
-interface AttributeHandlerInterface {}
