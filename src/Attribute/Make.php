623ea468d567d46cf9deed391ab18e5d495de445
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 02:34:02 +0200

    Restore Make source marker

--- src/Attribute/Make.php
+++ src/Attribute/Make.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Componenta\DI\Attribute;
 
 use Attribute;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Creates a fresh value through FactoryInterface. */
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class Make
+final readonly class Make implements ParameterSourceAttributeInterface
 {
     /** @param array<string|int, mixed> $params */
     public function __construct(
