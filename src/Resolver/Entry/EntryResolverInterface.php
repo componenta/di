2db6a070d3a8ebe1fb5ee0dfd56ff8cd12b96aa0
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-19 19:22:46 +0200

    Use root exception contract for entry resolvers

--- src/Resolver/Entry/EntryResolverInterface.php
+++ src/Resolver/Entry/EntryResolverInterface.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Componenta\DI\Resolver\Entry;
 
 use Componenta\DI\Exception\ExceptionInterface;
-use Componenta\DI\Exception\ResolutionException;
-use Psr\Container\NotFoundExceptionInterface;
 
 /** Resolves container entries by identifier. */
 interface EntryResolverInterface
@@ -15,8 +13,6 @@ interface EntryResolverInterface
 
     /**
      * @param array<string|int, mixed> $params
-     * @throws NotFoundExceptionInterface
-     * @throws ResolutionException
      * @throws ExceptionInterface
      */
     public function resolve(string $id, array $params = []): mixed;
