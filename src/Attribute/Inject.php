376c6a88587d21a5d6190dad8f1ee9cfbc267f33
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-20 12:15:26 +0200

    Restrict Inject to property targets

--- src/Attribute/Inject.php
+++ src/Attribute/Inject.php
@@ -6,6 +6,6 @@ namespace Componenta\DI\Attribute;
 
 use Attribute;
 
-/** Explicitly provides a target value from the container by declared type. */
-#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
+/** Explicitly injects a property from the container by declared type. */
+#[Attribute(Attribute::TARGET_PROPERTY)]
 final readonly class Inject {}
