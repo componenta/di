ee843f856cdde3f1a983e62583c521a286248b29
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 00:36:31 +0200

    Guard removal of implicit request context resolver

--- tests/V5/CurrentRequestAttributesSmokeTest.php
+++ tests/V5/CurrentRequestAttributesSmokeTest.php
@@ -7,6 +7,7 @@ namespace Componenta\DI\Tests\V5;
 use Attribute;
 use Componenta\DI\Attribute\CurrentRequest;
 use Componenta\DI\Attribute\CurrentUri;
+use Componenta\DI\ContainerBuilder;
 use ReflectionClass;
 
 it('declares current request context attributes for parameters only', function (): void {
@@ -15,4 +16,9 @@ it('declares current request context attributes for parameters only', function (
 
         expect($metadata->flags)->toBe(Attribute::TARGET_PARAMETER);
     }
+});
+
+it('does not expose an implicit request-context resolver contract', function (): void {
+    expect(defined(ContainerBuilder::class . '::PRIORITY_PARAM_REQUEST_CONTEXT'))->toBeFalse()
+        ->and(class_exists('Componenta\\DI\\Resolver\\Parameter\\RequestContextResolver'))->toBeFalse();
 });
