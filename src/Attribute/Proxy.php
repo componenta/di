be66a253e4fa2f390e4aafd8988ca37ac6cb8474
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 21:50:55 +0200

    Protect mapped Proxy parameter sources

--- src/Attribute/Proxy.php
+++ src/Attribute/Proxy.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Componenta\DI\Attribute;
 
 use Attribute;
+use Componenta\DI\Resolver\Parameter\ParameterSourceAttributeInterface;
 
 /** Marks a class or injection point for native virtual-proxy creation. */
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
-final readonly class Proxy
+final readonly class Proxy implements ParameterSourceAttributeInterface
 {
     /** @param class-string|null $class Concrete proxy class for an injection point. */
     public function __construct(public ?string $class = null) {}
